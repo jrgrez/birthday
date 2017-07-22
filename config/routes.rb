81995479460cf4d2ff25dcd1809bbfca783e7e1a
Joaquin Grez <joaquingrez@gmail.com>
2017-07-21 21:45:50 -0400

    Root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :birthdays
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'birthdays#index'
 end
